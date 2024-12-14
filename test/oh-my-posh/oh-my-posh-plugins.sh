025923bdb1453f7ab7f13f851c6049f8e9b51282
Fabian Schurig <fabian.a.schurig@gmail.com>
2024-12-14 16:20:09 +0100

    Eval config command (#5)
    * Eval oh-my-posh config inside the zshrc.
    
    * Bump version
    
    * Do not execute oh-my-posh when adding the line at the end of zshrc

--- test/oh-my-posh/oh-my-posh-plugins.sh
+++ test/oh-my-posh/oh-my-posh-plugins.sh
@@ -16,7 +16,7 @@ check "oh-my-posh is installed" oh-my-posh --version
 for user_home in /home/*; do
     if [ -d "$user_home" ]; then
         cat "$user_home/.zshrc"
-        check "oh-my-posh configuration is applied for $user_home" grep "oh-my-posh init zsh --config" "$user_home/.zshrc"
+        check "oh-my-posh configuration is applied for $user_home" grep "eval \"\$(oh-my-posh init zsh --config \$HOME/oh-my-posh-config.json)\"" "$user_home/.zshrc"
         check "zsh-autosuggestions plugin is installed for $user_home" grep "zsh-autosuggestions" "$user_home/.zshrc"
         check "zsh-syntax-highlighting plugin is installed for $user_home" grep "zsh-syntax-highlighting" "$user_home/.zshrc"
         check "oh-my-posh-config.json is similar to the remote file for $user_home" diff "$user_home/oh-my-posh-config.json" <(curl -s https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/pure.omp.json)
