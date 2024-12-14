a17e9bce69e6de9c9315478bdc4a0d7eaaa10718
Fabian Schurig <fabian.a.schurig@gmail.com>
2024-12-14 12:25:03 +0100

    Add options theme, plugins.

--- src/oh-my-posh/install.sh
+++ src/oh-my-posh/install.sh
@@ -11,110 +11,33 @@ if ! command -v curl &> /dev/null; then
     apt-get install -y curl
 fi
 
-if [ ! -d "/root/.oh-my-zsh" ]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
-
-for user in /home/*; do
-    if [ ! -d "$user/.oh-my-zsh" ]; then
-        sudo -u $(basename $user) sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    fi
+# Install oh-my-zsh for /root and each user in /home/*
+for user in /root /home/*; do
+  if [ ! -d "$user/.oh-my-zsh" ]; then
+    sudo -u $(basename $user) sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  fi
 done
 
-# Install required fonts using oh-my-posh font install meslo
+# Install oh-my-posh for root
 if ! command -v oh-my-posh &> /dev/null; then
-    curl -s https://ohmyposh.dev/install.sh | bash -s
-    oh-my-posh font install meslo
+  curl -s https://ohmyposh.dev/install.sh | bash -s
+  bash -c 'source ~/.profile && oh-my-posh font install meslo'
 fi
 
-# Configure oh-my-posh with the provided default configuration
-cat <<EOF > /etc/oh-my-posh-config.json
-{
-  "version": 2,
-  "final_space": true,
-  "console_title_template": "{{ .Shell }} in {{ .Folder }}",
-  "blocks": [
-    {
-      "type": "prompt",
-      "alignment": "left",
-      "newline": true,
-      "segments": [
-        {
-          "type": "path",
-          "style": "plain",
-          "background": "transparent",
-          "foreground": "blue",
-          "template": "{{ .Path }}",
-          "properties": {
-            "style": "full"
-          }
-        },
-        {
-          "type": "git",
-          "style": "plain",
-          "foreground": "p:grey",
-          "background": "transparent",
-          "template": " {{ .HEAD }}{{ if or (.Working.Changed) (.Staging.Changed) }}*{{ end }} <cyan>{{ if gt .Behind 0 }}⇣{{ end }}{{ if gt .Ahead 0 }}⇡{{ end }}</>",
-          "properties": {
-            "branch_icon": "",
-            "commit_icon": "@",
-            "fetch_status": true
-          }
-        }
-      ]
-    },
-    {
-      "type": "rprompt",
-      "overflow": "hidden",
-      "segments": [
-        {
-          "type": "executiontime",
-          "style": "plain",
-          "foreground": "yellow",
-          "background": "transparent",
-          "template": "{{ .FormattedMs }}",
-          "properties": {
-            "threshold": 5000
-          }
-        }
-      ]
-    },
-    {
-      "type": "prompt",
-      "alignment": "left",
-      "newline": true,
-      "segments": [
-        {
-          "type": "text",
-          "style": "plain",
-          "foreground_templates": [
-            "{{if gt .Code 0}}red{{end}}",
-            "{{if eq .Code 0}}magenta{{end}}"
-          ],
-          "background": "transparent",
-          "template": "❯"
-        }
-      ]
-    }
-  ],
-  "transient_prompt": {
-    "foreground_templates": [
-      "{{if gt .Code 0}}red{{end}}",
-      "{{if eq .Code 0}}magenta{{end}}"
-    ],
-    "background": "transparent",
-    "template": "❯ "
-  },
-  "secondary_prompt": {
-    "foreground": "magenta",
-    "background": "transparent",
-    "template": "❯❯ "
-  }
-}
-EOF
+# Fetch the oh-my-posh configuration file from the provided URL and copy it to /etc/skel
+THEME=${theme:-https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/powerlevel10k_rainbow.omp.json}
+curl -fsSL $THEME -o /etc/skel/oh-my-posh-config.json
 
-# Enable oh-my-posh instant prompt in ~/.zshrc for all users
-for user in /home/*; do
-    echo 'eval "$(oh-my-posh init zsh --config /etc/oh-my-posh-config.json)"' >> $user/.zshrc
+# Copy the oh-my-posh configuration file to each user's home directory and enable oh-my-posh instant prompt in ~/.zshrc
+for user in /root /home/*; do
+  cp /etc/skel/oh-my-posh-config.json $user/
+  chown $(basename $user):$(basename $user) $user/oh-my-posh-config.json
+  chmod 644 $user/oh-my-posh-config.json
+  runuser -l $(basename $user) -c 'echo "source ~/.profile && eval \"$(oh-my-posh init zsh --config $HOME/oh-my-posh-config.json)\"" >> $HOME/.zshrc'
 done
-echo 'eval "$(oh-my-posh init zsh --config /etc/oh-my-posh-config.json)"' >> /root/.zshrc
+
+# Install additional plugins for each user and /root
+PLUGINS=${plugins:-"git debian docker sudo vscode poetry postgres cp"}
+for user in /root /home/*; do
+  runuser -l $(basename $user) -c "sed -i '/^plugins=/ s/)/ $PLUGINS)/' \$HOME/.zshrc"
+done
