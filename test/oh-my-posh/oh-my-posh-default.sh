e58dfc8871ee5bacaf398ffbfb568f31134058ee
Fabian Schurig <fabian.a.schurig@gmail.com>
2024-12-14 15:26:27 +0100

    Check for all users

--- test/oh-my-posh/oh-my-posh-default.sh
+++ test/oh-my-posh/oh-my-posh-default.sh
@@ -13,8 +13,12 @@ check "zsh is installed" zsh --version
 check "curl is installed" curl --version
 check "oh-my-zsh is installed" [ -d "$HOME/.oh-my-zsh" ]
 check "oh-my-posh is installed" oh-my-posh --version
-cat $HOME/.zshrc
-check "oh-my-posh configuration is applied" grep "oh-my-posh init zsh --config" $HOME/.zshrc
+for user_home in /root /home/*; do
+    if [ -d "$user_home" ]; then
+        cat "$user_home/.zshrc"
+        check "oh-my-posh configuration is applied for $user_home" grep "oh-my-posh init zsh --config" "$user_home/.zshrc"
+    fi
+done
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
--- test/oh-my-posh/oh-my-posh-plugins.sh
+++ test/oh-my-posh/oh-my-posh-plugins.sh
@@ -13,10 +13,14 @@ check "zsh is installed" zsh --version
 check "curl is installed" curl --version
 check "oh-my-zsh is installed" [ -d "$HOME/.oh-my-zsh" ]
 check "oh-my-posh is installed" oh-my-posh --version
-cat $HOME/.zshrc
-check "oh-my-posh configuration is applied" grep "oh-my-posh init zsh --config" $HOME/.zshrc
-check "zsh-autosuggestions plugin is installed" grep "zsh-autosuggestions" $HOME/.zshrc
-check "zsh-syntax-highlighting plugin is installed" grep "zsh-syntax-highlighting" $HOME/.zshrc
+for user_home in /root /home/*; do
+    if [ -d "$user_home" ]; then
+        cat "$user_home/.zshrc"
+        check "oh-my-posh configuration is applied for $user_home" grep "oh-my-posh init zsh --config" "$user_home/.zshrc"
+        check "zsh-autosuggestions plugin is installed for $user_home" grep "zsh-autosuggestions" "$user_home/.zshrc"
+        check "zsh-syntax-highlighting plugin is installed for $user_home" grep "zsh-syntax-highlighting" "$user_home/.zshrc"
+    fi
+done
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
